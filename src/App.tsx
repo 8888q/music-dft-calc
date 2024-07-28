cbd1e2ccfc11a81480e973ce3c16daaf7865cd10
Jake <jg1267@wildcats.unh.edu>
2024-07-27 20:42:25 -0700

    precision checking, degree column

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { useState } from 'react';
 import './App.css';
 import { Box, Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from '@mui/material';
 
-type complexNumber = readonly [number, number];
-const decimalPrecision: number = 3;
+type complexNumber = [number, number];
+const decimalPrecision: number = 6;
+const precisionCutoff: number = 0.0000001;
 
 function App() {
   const [characteristicFunctionInput, setCharacteristicFunctionInput] = useState("0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0");
@@ -47,7 +48,9 @@ function App() {
     }
 
     const complexList: complexNumber[] = [];
-    for( let i = 0; i < 12; i++ ){
+    for( let i = 0; i < 8; i++ ){
+      if( realParts[i] < precisionCutoff && realParts[i] > -precisionCutoff ) realParts[i] = 0;
+      if( imaginaryParts[i] < precisionCutoff && imaginaryParts[i] > -precisionCutoff ) imaginaryParts[i] = 0;
       complexList.push([realParts[i], imaginaryParts[i]])
     }
 
@@ -55,20 +58,21 @@ function App() {
   }
 
   return (
-    <Box display="flex" flexDirection="column" gap="32px" alignItems="center" paddingTop="100px" minHeight="100vh" width="100vw" bgcolor="#fce5de" >
-      <Typography variant='h3'>Characteristic Function DFT Calculator</Typography>
+    <Box display="flex" flexDirection="column" gap="32px" alignItems="center" minHeight="100vh" width="100vw" bgcolor="#fce5de" >
+      <Typography variant='h3' paddingTop="64px">Characteristic Function DFT Calculator</Typography>
       <Box display="flex" flexDirection="row" gap="32px" width="40vw" >
         <TextField fullWidth error={inputError} helperText={errorText} label="Characteristic Function" value={characteristicFunctionInput} onChange={handlePitchClassInputChange} />
         <Button variant='contained' onClick={() => computeDFT(pitchClass)} >Calculate</Button>
       </Box>
-      <TableContainer sx={{width: "60vw"}}>
+      <TableContainer sx={{width: "60vw" }}>
         <Table>
           <TableHead>
             <TableRow>
               <TableCell>Index</TableCell>
               <TableCell align="right">Magnitude</TableCell>
               <TableCell align="right">Magnitude<sup>2</sup></TableCell>
-              <TableCell align="right">Theta</TableCell>
+              <TableCell align="right">Angle &Theta; (radians)</TableCell>
+              <TableCell align="right">Angle &Theta; (degrees)</TableCell>
               <TableCell align="right">Real Part</TableCell>
               <TableCell align="right">Imaginary Part</TableCell>
             </TableRow>
@@ -76,15 +80,19 @@ function App() {
           <TableBody>
             {
               complexNumbers.map( z => {
+
                 const magnitudeSquared = z[0] * z[0] + z[1] * z[1];
-                const theta = Math.atan2(z[1], z[0]);
+                let theta = Math.atan2(z[1], z[0]);
+                if( theta < precisionCutoff && theta > -precisionCutoff ) theta = 0;
+                if( theta < 0 ) theta += 2 * Math.PI;
 
                 return (
                   <TableRow>
                     <TableCell>{complexNumbers.findIndex((x) => x == z)}</TableCell>
                     <TableCell align="right">{Math.sqrt( magnitudeSquared ).toPrecision(2)}</TableCell>
                     <TableCell align="right">{magnitudeSquared.toPrecision(decimalPrecision)}</TableCell>
-                    <TableCell align="right">{theta.toPrecision(decimalPrecision)}</TableCell>
+                    <TableCell align="right">{theta.toPrecision(decimalPrecision) + " rad"}</TableCell>
+                    <TableCell align="right">{(theta * 180 / Math.PI).toPrecision(decimalPrecision)}&deg;</TableCell>
                     <TableCell align="right">{z[0].toPrecision(decimalPrecision)}</TableCell>
                     <TableCell align="right">{z[1].toPrecision(decimalPrecision)}</TableCell>
                   </TableRow>
